ac06b686120e22b6f62af7f229675113aac09831
fajrulum <mfajrululum@gmail.com>
2021-09-24 14:57:06 +0700

    tugas pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_CONTROLLER
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhamad Fajrul Ulum
+            Saya tinggal di daerah Pasar Minggu
+            olah raga yang saya sukai adalah
+            Sepakbola";
+    }
+}
